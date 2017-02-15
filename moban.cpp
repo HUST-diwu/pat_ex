aa73855840781d23425ed0347a066f613ae5395c
Diwu <cnhustwd@gmail.com>
2017-02-15 19:10:49 +0800

    still unhandel error

--- moban.cpp
+++ moban.cpp
@@ -4,13 +4,15 @@
 #include <stack>
 #include <map>
 using namespace std;
-vector<node> States(8*7*6*5*4*3*2*1);
-map<vector<char>,int> pre_index;//no pre 
 struct node{
 	vector<char> v;
 	char type;
-	node():v(v),type(type){};
-}
+	node(){}
+	node(vector<char> v,char type):v(v),type(type){};
+};
+vector<node> States(8*7*6*5*4*3*2*1);
+map<vector<char>,int> pre_index;//no pre 
+std::vector<char> _END(8),start(8);
 void BFS()
 {
 	pre_index.clear();
@@ -26,31 +28,31 @@ void BFS()
 			vector<char> new_state=temp;
 			if(i=='A')
 			{
-				for(int j=0;j<4;j++)
-				{
-					swap(new_state[j],new_state[j+4]);
-				}
+				swap(new_state[0],new_state[7]);swap(new_state[1],new_state[6]);swap(new_state[2],new_state[5]);
+				swap(new_state[3],new_state[4]);
 			}
 			else if(i=='B')
 			{
-				char t1=new_state[3],t2=new_state[7];
-				for(int j=7;j>0;j--)
+				char t1=new_state[3],t2=new_state[4];
+				for(int j=3;j>0;j--)
 					new_state[j]=new_state[j-1];
-				new_state[0]=t1,new_state[4]=t2;
+				for(int j=5;j<=7;j++)
+					new_state[j-1]=new_state[j];
+				new_state[0]=t1,new_state[7]=t2;
 			}
 			else{
-				char t1=new_state[1],t2=new_state[2],t3=new_state[6],t4=new_state[5];
+				char t1=new_state[1],t2=new_state[2],t3=new_state[5],t4=new_state[6];
 				new_state[1]=t4;
 				new_state[2]=t1;
-				new_state[6]=t2;
-				new_state[5]=t3;
+				new_state[5]=t2;
+				new_state[6]=t3;
 			}
 			if(pre_index.find(new_state)==pre_index.end())//new state has never been searched
 			{
 				States[cnt++]=node(temp,i);
 				pre_index[new_state]=cnt-1;
 				Q.push(new_state);
-				if(new_state==end_state)
+				if(new_state==_END)
 				{
 					return;
 				}
@@ -60,13 +62,12 @@ void BFS()
 }
 int main()
 {
-	std::vector<char> end(8),start(8);
 	for(int i=0;i<8;i++) start[i]=i+1;
-	while(scanf("%d%d%d%d%d%d%d%d",&end[0],&end[1],&end[2],&end[3],&end[4],&end[5],&end[6],&end[7])==8);
+	while(scanf("%d%d%d%d%d%d%d%d",&_END[0],&_END[1],&_END[2],&_END[3],&_END[4],&_END[5],&_END[6],&_END[7])!=EOF)
 	{
 		BFS();
 		stack<char> s;
-		std::vector<char> temp_state=end;
+		std::vector<char> temp_state=_END;
 		while(true)
 		{
 			int pos=pre_index[temp_state];
